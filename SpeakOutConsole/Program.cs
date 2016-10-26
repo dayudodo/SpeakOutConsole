f0c16750f2ac526928de0a9181ea4c55d790a6c7
dayudodo <dayudodo@gmail.com>
2016-10-26 19:49:36 +0800

    command line make wav

--- SpeakOutConsole/Program.cs
+++ SpeakOutConsole/Program.cs
@@ -61,10 +61,10 @@ namespace SampleSynthesis
             //Node node = Node.FromFile("biposition_2.yml");
             //node = Node.Parse("- item1\n- item2\n");
             //Console.WriteLine(node);
+                        
+            //string sampleText = ConfigurationManager.AppSettings["sampleText"];
+            //string outputWaveFileName = ConfigurationManager.AppSettings["outputWaveFileName"];
 
-            Speaker sp = new Speaker();
-            string sampleText = ConfigurationManager.AppSettings["sampleText"];
-            string outputWaveFileName = ConfigurationManager.AppSettings["outputWaveFileName"];
             string inputFileName = ConfigurationManager.AppSettings["inputFileName"];
             //sp.speakToWaveFile(sampleText, outputWaveFileName);
 
@@ -72,31 +72,49 @@ namespace SampleSynthesis
             //去掉重复的字符串
             readTexts = (from item in readTexts
                          select item).Distinct().ToArray();
-
-            foreach (String read_text in readTexts)
+            //如果没有命令行，就读取yml文件来生成，如果有参数，那么就生成需要朗读的句子们
+            if (args.Length == 0)
             {
-                //在yml文件中，课程文件只有一级，需要发音的都是以-开头，示例文件：biAnimal.yml
-                if (read_text.StartsWith("-"))
+                foreach (String read_text in readTexts)
                 {
-                    string fileName = (read_text.Substring(2) + ".wav").ToLower();
-                    //写文件的时候使用小写，以在linux上更好的使用。
-                    if (File.Exists(fileName))
+                    //在yml文件中，课程文件只有一级，需要发音的都是以-开头，示例文件：biAnimal.yml
+                    if (read_text.StartsWith("-"))
                     {
-                        Console.WriteLine("!!! file already exit:{0} !!!", fileName);
-                    }
-                    else
-                    {
-                        sp.speakToWaveFile(read_text.Substring(2), fileName);
-                        //Console.WriteLine(fileName);
-                        Console.WriteLine("{0} is TTS reading...", fileName);
+                        string phrase = read_text.Substring(2);
+                        string fileName = (read_text.Substring(2) + ".wav").ToLower();
+                        
+                        wavToFile(phrase, fileName);
                     }
                 }
             }
+            else
+            {
+                foreach (var item in args)
+                {
+                    string fileName = (item + ".wav").ToLower();
+                    wavToFile(item, fileName);
+                }
+            }
+
 
             //Console.WriteLine();
             Console.WriteLine("Press any key to exit...");
             Console.ReadKey();
         }
-
+        static void wavToFile(string phrase, string filename)
+        {
+            Speaker sp = new Speaker();
+            //写文件的时候使用小写，以在linux上更好的使用，并不需要头字母大字
+            if (File.Exists(filename))
+            {
+                Console.WriteLine("!!! file already exit:{0} !!!", filename);
+            }
+            else
+            {
+                sp.speakToWaveFile(phrase, filename);
+                //Console.WriteLine(fileName);
+                Console.WriteLine("{0} TTS spoke. ", filename);
+            }
+        }
     }
 }
